4054c78202460a221e66b42ed05201759f871a5d
Ben Morales <bmorales4@unl.edu>
2020-08-20 14:50:34 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author:  Benjamin Morales
+ * Date:    2020
+ * 
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Benjamin Morales\n");
+     printf("Computer Science and Mathematics\n");
+     
+     return 0;
+ }
