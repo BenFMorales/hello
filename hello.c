3a446ce2843eed2655009893fad50f946d6f6a5d
odalys garcia <ogarcia2@huskers.unl.edu>
2020-08-20 17:43:35 +0000

    added name and major to Bens repository

--- hello.c
+++ hello.c
@@ -1,16 +1,18 @@
 /**
  * Author:  Benjamin Morales
  * Date:    2020
- * 
+ *
  * A simple hello world program
  */
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
-     
+
      printf("Benjamin Morales!\n");
      printf("Computer Science\n");
-     
+     printf("Odalys Garcia\n");
+     printf("Computer Engineering\n");
+
      return 0;
  }
